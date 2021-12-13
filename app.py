11c8a08586623117eb5097063164f25e247feabf
Nathanael Kempel <89422478+nkempel@users.noreply.github.com>
2021-12-13 11:02:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ AWS_REGION = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/pla
 def fetch_meme_string():
     headers = {
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'cookie': '__utmc=71379083; iflipsess=u2h951epfj9p581vvsh280hv89; __utma=71379083.734663800.1635440230.1636492023.1636653592.7; __utmz=71379083.1636653592.7.6.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmt=1; __utmb=71379083.2.9.1636653592',
+        'cookie': '__utmc=71379083; iflipsess=9e78gc9ghkcicd6385p31uug6e; __utma=71379083.1923845604.1639414590.1639414590.1639414590.1; __utmz=71379083.1639414590.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=71379083.4.9.1639414622432',
     }
 
     post_request = 'use_openai=0&meme_id=61520&init_text=&__tok=XEnWK0Pkd65mdCUZPQoUXNAnUOokKOmBvuxjiHEK+0U=&__cookie_enabled=1'
