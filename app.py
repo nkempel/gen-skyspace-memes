3a9fdd26d1d694111dd49ba87d11c4d2696cdfd5
Nathanael Kempel <89422478+nkempel@users.noreply.github.com>
2021-12-17 09:49:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ AWS_REGION = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/pla
 def fetch_meme_string():
     headers = {
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'cookie': '__utmc=71379083; iflipsess=u2h951epfj9p581vvsh280hv89; __utma=71379083.1923845604.1639414590.1639414590.1639425117.2; __utmz=71379083.1639414590.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmb=71379083.2.9.1639425117',
+        'cookie': '__utmc=71379083; iflipsess=u2h951epfj9p581vvsh280hv89; __utma=71379083.1923845604.1639414590.1639598187.1639756093.4; __utmz=71379083.1639414590.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmb=71379083.2.9.1639756093',
     }
 
-    post_request = 'use_openai=0&meme_id=61520&init_text=&__tok=M8wbV9+QxD5Xc4gmNIsNJfUWQZeNKGzmZ5uO6eN1boI=&__cookie_enabled=1'
+    post_request = 'use_openai=0&meme_id=61520&init_text=&__tok=N9xSLA+YjJ2VPn7eHE92Seb2wczr+R2ND+ccfeqB24U=&__cookie_enabled=1'
     response = requests.post(
         "https://imgflip.com/ajax_ai_meme",
         data=post_request,
